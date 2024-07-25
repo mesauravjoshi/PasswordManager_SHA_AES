4953a2a425db5f5ff7fb42b993b6d6309dce1e94
Saurav Joshi <mesauravjoshii@gmail.com>
2024-07-25 21:05:58 +0530

    first_display_message.py

--- first_display_message.py
+++ first_display_message.py
@@ -2,7 +2,7 @@ def first_display_message():
     print('''
      ========================================================================================
     |                                                                                         |
-    |       Password Manager Security Guidelines:                                             |
+    |                   INSTRUCTIONS                                                          |
     |    1. Please set your password for the Password Manager.                                |   
     |    2. To retrieve your passwords, enter your Password Manager password.                 |
     |                                                                                         |
