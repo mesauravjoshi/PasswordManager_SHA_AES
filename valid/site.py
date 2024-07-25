9f2dc44918e9fb8e59d748dea7409104e7c7036f
Saurav Joshi <mesauravjoshii@gmail.com>
2024-07-25 21:00:35 +0530

    site.py

--- valid/site.py
+++ valid/site.py
@@ -3,10 +3,9 @@ def validate_site():
     while attempts < 3:
         username = input("Enter a site : ")
         if username.isdigit():
-            print("Website name should not be only numeric. Please enter a valid username.")
+            print("Error: Website name should not be only numeric. Please enter a valid username.")
             attempts += 1
         else:
-            print("Username is valid.")
             return username
     if attempts == 3:
         print("Maximum attempts reached. Exiting.")
